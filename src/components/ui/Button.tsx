2f98da3415efbb50f9ef8b6fc7d8c2955c179e68
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-04-13 20:59:18 +0530

    added db

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,7 @@
-import { cva } from "class-variance-authority";
-import { FC } from "react";
-
-interface ButtonProps {}
+import { cn } from '@/lib/utils';
+import { cva, VariantProps } from 'class-variance-authority'
+import { Loader2 } from 'lucide-react';
+import {ButtonHTMLAttributes, FC } from "react";
 
 export const buttonVariants = cva(
   "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
@@ -24,8 +24,27 @@ export const buttonVariants = cva(
   }
 );
 
-const Button: FC<ButtonProps> = ({}) => {
-  return <div>page</div>;
-};
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>,  VariantProps<typeof buttonVariants> {
+    isLoading?: boolean
+  }
 
-export default Button;
+const Button: FC<ButtonProps> = ({
+    className,
+    children,
+    variant,
+    isLoading,
+    size,
+    ...props
+  }) => {
+    return (
+      <button
+        className={cn(buttonVariants({ variant, size, className }))}
+        disabled={isLoading}
+        {...props}>
+        {isLoading ? <Loader2 className='mr-2 h-4 w-4 animate-spin' /> : null}
+        {children}
+      </button>
+    )
+  }
+  
+  export default Button
