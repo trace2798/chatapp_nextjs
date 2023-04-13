1769b23b9270ac19359c9a97ca9fc81957c3e007
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-04-13 22:18:12 +0530

    Google login added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import { db } from '@/lib/db'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default async function Home() {
+
+  await db.set('hello', 'hello')
   return (
-    <div className='text-9xl '>Hello World</div>
+    <div className='text-9xl text-blue-400'>Hello World</div>
   )
 }
