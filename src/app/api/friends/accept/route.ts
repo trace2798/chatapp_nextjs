1ca4f041e93e02caa83d55b5ae79b1111660b195
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-04-14 19:25:39 +0530

    accept route

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,4 +1,55 @@
-export async function GET(request: Request) {
-    return new Response('Hello, Next.js!')
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+
+    //Validate if the id is string.
+    const { id: idToAdd } = z.object({ id: z.string() }).parse(body);
+    //getting the session
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    // verify both users are not already friends
+    const isAlreadyFriends = await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`,
+      idToAdd
+    );
+
+    if (isAlreadyFriends) {
+      return new Response("Already friends", { status: 400 });
+    }
+
+    const hasFriendRequest = await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:incoming_friend_requests`,
+      idToAdd
+    );
+
+    if (!hasFriendRequest) {
+      return new Response("No friend request", { status: 400 });
+    }
+
+    await db.sadd(`user:${session.user.id}:friends`, idToAdd);
+    await db.sadd(`user:${idToAdd}:friends`, session.user.id),
+    db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd),
+
+    return new Response("OK");
+  } catch (error) {
+    console.log(error);
+
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request payload", { status: 422 });
+    }
+
+    return new Response("Invalid request", { status: 400 });
   }
-  
+}
